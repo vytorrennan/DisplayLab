8fa1fd8f01980d122e3575996b731aeb6d90e8ca
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-07 14:14:59 -0300

    Criando app novo para o sistema de conta, e corrigindo o nome do app de contentManagemment para contentManagement

--- DisplayLab/urls.py
+++ DisplayLab/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('main.urls')),
     path("", include('projetos.urls')),
-    path("contentManagemment/", include('contentManagemment.urls')),
+    path("contentManagement/", include('contentManagement.urls')),
 ]
