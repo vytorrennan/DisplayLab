1dc4db93d8476f935688242bac947c79b7efeab7
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-07 16:07:33 -0300

    adicionando / na frente de todas as urls pra concertar o bug de digitar com / e n ir pra pagina

--- contentManagement/urls.py
+++ contentManagement/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("novoProjeto", views.novoProjeto, name="novoProjeto"),
+    path("novoProjeto/", views.novoProjeto, name="novoProjeto"),
 ]
--- projetos/urls.py
+++ projetos/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path("projetos", views.projetos, name="projetos"),
+    path("projetos/", views.projetos, name="projetos"),
 ]
