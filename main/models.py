d91cafa46ee4f01cebf1fcd93ec0ae6f6bec5bf5
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-06 22:11:57 -0300

    criando models das paginas Home, Projetos, Revista, Institucional e Quem Somos

--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ItensDoCarousel(models.Model):
+    imagem = models.CharField(max_length=512, unique=True),
+    url = models.CharField(max_length=512, unique=True),
+
+
+class Membros(models.Model):
+    id = models.AutoField(primary_key=True),
+    deletado = models.BooleanField(),
+    nome = models.CharField(max_length=200),
+    categoria = models.CharField(max_length=200),
+    linkedin = models.CharField(max_length=512),
+    descricao  = models.CharField(max_length=200),
--- projetos/models.py
+++ projetos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Projetos(models.Model):
+    id = models.AutoField(primary_key=True),
+    deletado = models.BooleanField(),
+    gosteis = models.PositiveIntegerField(),
+    url = models.CharField(max_length=512, unique=True),
+    titulo = models.CharField(max_length=512, unique=True),
+    capa = models.CharField(max_length=512),
+    resumo  = models.CharField(max_length=2048),
+    pagina = models.TextField(),
