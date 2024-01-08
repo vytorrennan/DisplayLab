d3c138601d84dc04400928e5a0475b43b3bcace4
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-08 18:34:29 -0300

    mudado o form.py do novoProjeto pra modelsForm

--- contentManagement/novoProjeto.py
+++ contentManagement/novoProjeto.py
@@ -1,9 +1,13 @@
 from django import forms
+from projetos.models import Projetos
 
-
-class novoProjetoForm(forms.Form):
-    url = forms.CharField(label="Url", max_length=250, widget=forms.TextInput())
-    titulo = forms.CharField(label="Titulo", max_length=250, widget=forms.TextInput())
-    capa = forms.CharField(label="Capa", max_length=512, widget=forms.TextInput(attrs={"name":"capa"}))
-    resumo = forms.CharField(label="Resumo", max_length=1000, widget=forms.Textarea(attrs={"rows":"6"}))
-    pagina = forms.CharField(label="Pagina", widget=forms.Textarea(attrs={"rows":"30"}))
+class novoProjetoForm(forms.ModelForm):
+    class Meta:
+        model = Projetos
+        fields = ["url", "titulo", "capa", "resumo", "pagina"]
+    
+    #url = forms.CharField(label="Url", max_length=250, widget=forms.TextInput())
+    #titulo = forms.CharField(label="Titulo", max_length=250, widget=forms.TextInput())
+    #capa = forms.CharField(label="Capa", max_length=512, widget=forms.TextInput())
+    #resumo = forms.CharField(label="Resumo", max_length=1000, widget=forms.Textarea(attrs={"rows":"6"}))
+    #pagina = forms.CharField(label="Pagina", widget=forms.Textarea(attrs={"rows":"30"}))
