c0137e7b15099145ad21590ab23073486cd87e82
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-09 00:13:38 -0300

    Criado novoPostRevista

---
+++ contentManagement/forms/novoPostRevista.py
@@ -0,0 +1,13 @@
+from django import forms
+from revista.models import Revista
+from tinymce.widgets import TinyMCE
+
+
+class novoPostRevistaForm(forms.ModelForm):
+    class Meta:
+        model = Revista
+        fields = ["url", "titulo", "capa", "resumo", "pagina"]
+        widgets = {
+            'resumo': forms.Textarea(),
+            'pagina': TinyMCE()
+        }
