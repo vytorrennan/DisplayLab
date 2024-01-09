bc48f7e65af49274e37358310be6216a829425af
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-08 23:30:08 -0300

    criação do form de novoItemCarousel e adicionado o link nele e no novoProjeto

--- contentManagement/urls.py
+++ contentManagement/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.contentManagement, name="contentManagement"),
+    path("novoItemCarousel/", views.novoItemCarousel, name="novoItemCarousel"),
     path("novoProjeto/", views.novoProjeto, name="novoProjeto"),
 ]
