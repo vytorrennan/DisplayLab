f50087f776b8c693480a4d65ff1bf751b8067b8a
Vytor Rennan <crosdeadbr@gmail.com>
2024-01-15 21:20:27 -0300

    Adicionado paginação na pagina de revista

--- revista/views.py
+++ revista/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Revista
 
 # Create your views here.
@@ -6,7 +7,21 @@ from .models import Revista
 
 def revista(request):
     posts = Revista.objects.filter(oculto=False).order_by("-edicao", "-dataHora")
-    context = {"posts": posts}
+    paginator = Paginator(posts, 12)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    quantasPaginasMostrar = 9
+    if (page_obj.number <= page_obj.paginator.num_pages - (quantasPaginasMostrar + 1)):
+        if (page_obj.number == 1):
+            rangePages = range(page_obj.number, page_obj.number + (quantasPaginasMostrar + 1))
+        else:
+            rangePages = range(page_obj.number - 1, page_obj.number + (quantasPaginasMostrar + 1))
+    else:
+        if (page_obj.number == 1):
+            rangePages = range(page_obj.number, page_obj.paginator.num_pages + 1)
+        else:
+            rangePages = range(page_obj.number - 1, page_obj.paginator.num_pages + 1)
+    context = {"page_obj": page_obj, "rangePages": rangePages}
     return render(request, "revista.html", context)
 
 
